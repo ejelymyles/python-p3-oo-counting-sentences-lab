70791b2b19f2242030dd047f8e22b7d302482af6
Ej Myles <ejelymyles@gmail.com>
2023-10-16 00:36:11 -0700

    finish lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
+  def __init__(self, value=""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+
+  def is_sentence(self):
+    return self.value.endswith('.')
+
+  def is_question(self):
+    return self.value.endswith('?')
+
+  def is_exclamation(self):
+    return self.value.endswith('!')
+
+  def count_sentences(self):
+        separator_pattern = r"[.!?]+"
+        sentences = re.split(separator_pattern, self.value)
+        sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(sentences)
+
+
   pass
